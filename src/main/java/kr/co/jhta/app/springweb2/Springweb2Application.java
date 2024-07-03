6aba345cd8b0e66071df9d7d313dbd809c6774ec
KS <dagda@hanafos.com>
2024-07-03 17:15:07 +0900

    1

---
+++ src/main/java/kr/co/jhta/app/springweb2/Springweb2Application.java
@@ -0,0 +1,13 @@
+package kr.co.jhta.app.springweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springweb2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/jhta/app/springweb2/Springweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jhta.app.springweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springweb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
